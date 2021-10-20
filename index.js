94b8d4bbfb1390abef0b9fe50e7457ff7b7a1752
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-10-20 11:20:33 +0200

    complete w/o bonus

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 $(function(){
   var charGot = $("#char-got")
-  var input = $("input").val()
   var chars = []
   
   $.ajax({
     url: "https://thronesapi.com/api/v2/Characters",
     success: function(data) {
       display(data)
-      chars.push(data)
-      console.log(chars)
+      chars = data
     }
   })
   
   function display(array) {
-    var html = array.forEach(function(element) {
+    charGot.empty()
+    array.forEach(function(element) {
       charGot.append(`
         <div class="charbox">
           <div class="image-box">
@@ -25,32 +24,16 @@ $(function(){
       `)
     })
   }
-
-  $('#form').submit(function(e) {
+  
+  
+  $('form').submit(function(e) {
     e.preventDefault()
 
+    var input = $("input").val()
     var filteredChars = chars.filter(function(char){
       return char.fullName.toLowerCase().includes(input)
-    }) 
-    console.log(input)
-    console.log(filteredChars)
+    })
     display(filteredChars)
-
+    
   })
-
-
-
-  // $.ajax({
-  //   url: "https://thronesapi.com/api/v2/Characters",
-  //   success: function(datas) {
-  //     $("button").click(function(){
-  //       var filteredData = datas.filter(function(data){
-  //         return data.fullName = input
-  //       })
-  //       display(filteredData)
-
-  //     })
-  //   }
-  // })
-
 })
